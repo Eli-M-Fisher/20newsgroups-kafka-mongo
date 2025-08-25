c03ce0fab28a4a4b5b77e2259bb48cc56a037d83
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-25 16:58:17 +0300

    release/i finished implementing the entire class dataset

--- publisher/app/dataset.py
+++ publisher/app/dataset.py
@@ -4,9 +4,20 @@ Here I handle loading (and also sampling) data from a 20newsgroups dataset.
 from sklearn.datasets import fetch_20newsgroups
 import random
 
-INTERESTING = []
 
-NOT_INTERESTING = []
+INTERESTING = [
+    "alt.atheism", "comp.graphics", "comp.os.ms-windows.misc",
+    "comp.sys.ibm.pc.hardware", "comp.sys.mac.hardware",
+    "comp.windows.x", "misc.forsale", "rec.autos",
+    "rec.motorcycles", "rec.sport.baseball"
+]
+
+NOT_INTERESTING = [
+    "rec.sport.hockey", "sci.crypt", "sci.electronics",
+    "sci.med", "sci.space", "soc.religion.christian",
+    "talk.politics.guns", "talk.politics.mideast",
+    "talk.politics.misc", "talk.religion.misc"
+]
 
 def load_messages():
     """
@@ -14,4 +25,12 @@ def load_messages():
     
     and then a list of tuples (subject, text) is returned
     """
+    interesting_data = fetch_20newsgroups(subset="all", categories=INTERESTING)
+    not_interesting_data = fetch_20newsgroups(subset="all", categories=NOT_INTERESTING)
+
+    messages = []
+    for _ in range(n):
+        messages.append(("interesting", random.choice(interesting_data.data)))
+        messages.append(("not_interesting", random.choice(not_interesting_data.data)))
+
     return messages
