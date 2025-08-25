a9b50502815e4dad3d18ea32d0725dbce64cbe84
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-25 16:50:00 +0300

    release/i finished implementing the entire class utils logger

--- publisher/app/utils.py
+++ publisher/app/utils.py
@@ -5,5 +5,12 @@ import logging
 
 
 def get_logger(name: str):
-
+    logger = logging.getLogger(name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "[%(asctime)s] %(levelname)s in %(name)s: %(message)s"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
     return logger
